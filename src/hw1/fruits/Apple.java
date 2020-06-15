f1e852822a6795a917412e961af39990d13d3e6b
Dmitriy Nogin <tu002ndg@gmail.com>
2020-06-15 12:36:46 +0300

    ++hw1

---
+++ src/hw1/fruits/Apple.java
@@ -0,0 +1,13 @@
+package hw1.fruits;
+
+public class Apple extends Fruit {
+
+    public Apple() {
+        this(1.0f);
+    }
+
+    public Apple(float weight) {
+        super(weight);
+    }
+
+}
---
+++ src/hw1/fruits/Orange.java
@@ -0,0 +1,12 @@
+package hw1.fruits;
+
+public class Orange extends Fruit {
+
+    public Orange() {
+        this(1.5f);
+    }
+
+    public Orange(float weight) {
+        super(weight);
+    }
+}
